c2887f3c7a84196474845f7d93f97dd75c7035fd
gabrielhdt <gabrielhondet@gmail.com>
2018-02-20 17:19:26 +0100

    (~) corrected index use

--- src/sudoku.ml
+++ src/sudoku.ml
@@ -11,19 +11,25 @@ type instance = v Xmap.t
 
 let empty = Xmap.empty
 
+(* Length of the side of a subsquare *)
+let size = 3
+let _s = size * size (* Convenience *)
+
 (* Builds a list of all vars *)
 let vars =
   let rec loop l c acc =
-    if (l, c) >= (9, 9) then acc
-    else if c >= 9 then loop (succ l) 0 ((l, c) :: acc)
+    if (l, c) >= (_s - 1, _s - 1) then (_s - 1, _s - 1) :: acc
+    else if c >= _s - 1 then loop (succ l) 0 ((l, c) :: acc)
     else loop l (succ c) ((l, c) :: acc)
   in
   loop 0 0 []
 
 (* Returns coordinates of subsquare of var *)
-let group_of_var = function (l, c) -> l / 3, c / 3
+let group_of_var = function (l, c) -> l / size, c / size
 
-let domain x = [ 1 ; 2 ; 3 ; 4 ; 5 ; 6 ; 7 ; 8 ; 9 ]
+let domain x =
+  let rec loop k acc = if k > _s then acc else loop (succ k) (k :: acc) in
+  loop 1 []
 
 let union inst var value = Xmap.add var value inst
 
@@ -43,15 +49,14 @@ let consistent sudoku =
       feasible k elt sudoku_nok && acc) sudoku true
 
 let print sudoku =
-  print_endline " ---|---|---" ;
-  for line = 1 to 9 do
-    print_string "|" ;
-    for col = 1 to 9 do
+  print_endline "Resulting すどく" ;
+  for line = 0 to _s - 1 do
+    for col = 0 to _s - 1 do
       if Xmap.mem (line, col) sudoku
       then Printf.printf "%d" (Xmap.find (line, col) sudoku)
       else Printf.printf "n" ;
-      if col mod 3 = 0 then print_string "|"
+      if (col + 1) mod size = 0 then print_string " "
     done ;
     print_newline () ;
-    if line mod 3 = 0 then print_endline "|---|---|---|"
+    if (line + 1) mod size = 0 then print_newline ()
   done

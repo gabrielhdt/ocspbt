25ebc280e209049356270517671c084d186a0696
gabrielhdt <gabrielhondet@gmail.com>
2018-02-22 18:35:11 +0100

    (+) More and more abstraction ... perhaps too much

--- src/backtrack.ml
+++ src/backtrack.ml
@@ -1,46 +1,72 @@
 module type CSPS = sig
-  type x
 
-  type v
+  module type VARS = sig
+    type t
 
-  type instance
+    type set
 
-  val empty : instance
+    val init : set
 
-  val vars : x list
+    val empty : set
 
-  val feasible : x -> v -> instance -> bool
+    val next : set -> t * set
+  end
 
-  val consistent : instance -> bool
+  module type VALS = sig
+    type t
 
-  val union : instance -> x -> v -> instance
+    type set
 
-  val domain : x -> v list
+    val empty : set
 
-  val print : instance -> unit
+    val next : set -> t * set
+  end
+
+  module type INSTS = sig
+    type t
+
+    val empty : t
+  end
+
+  module Var : VARS
+
+  module Val : VALS
+
+  module Instance : INSTS
+
+  val feasible : Var.t -> Val.t -> Instance.t -> bool
+
+  val consistent : Instance.t -> bool
+
+  val union : Instance.t -> Var.t -> Val.t -> Instance.t
+
+  val domain : Var.t -> Val.set
+
+  val print : Instance.t -> unit
 end
 
 module Make (Csp : CSPS) = struct
 
-  let rec bt_aux (v : Csp.x list) (a : Csp.instance option) =
+  let rec bt_aux (v : Csp.Var.set) (a : Csp.Instance.t option) =
     let instance = match a with (* Extract instance *)
       | None -> failwith "何"
       | Some k -> k
     in
-    match v with
-    | [] -> a (* If no more variable, everything is finished, return *)
-    | hd :: tl ->
-        (* Loops over values of var [hd] to grow tree *)
-        let rec loop = function
-          | [] -> None (* No feasible new instance found *)
-          | x :: rxs ->
-              if Csp.feasible hd x instance
-              then match bt_aux tl (Some (Csp.union instance hd x)) with
-                (* If the new instance is not feasible, try another value *)
-                | None -> loop rxs
-                | Some j -> Some j (* Good instance, return it *)
-              else loop rxs
-        in loop (Csp.domain hd)
+    if v = Csp.Var.empty
+    then a (* If no more variable, everything is finished, return *)
+    else let hd, tl = Csp.Var.next v in
+      (* Loops over values of var [hd] to grow tree *)
+      let rec loop values =
+        if values = Csp.Val.empty
+        then None (* No feasible new instance found *)
+        else let x, rxs = Csp.Val.next values in
+          if Csp.feasible hd x instance
+          then match bt_aux tl (Some (Csp.union instance hd x)) with
+            (* If the new instance is not feasible, try another value *)
+            | None -> loop rxs
+            | Some j -> Some j (* Good instance, return it *)
+          else loop rxs
+      in loop (Csp.domain hd)
 
   let bt v a =
     if Csp.consistent a then
--- src/backtrack.mli
+++ src/backtrack.mli
@@ -1,44 +1,77 @@
 (** Functorial interface to the csp solver using backtrack *)
 
+(** Submodule of the CSP, handles variables *)
+module type VARS = sig
+
+  (** The type of variables *)
+  type t
+
+  (** Type of a group of variables *)
+  type set
+
+  (** The initial set of variables to instantiate *)
+  val init : set
+
+  (** The empty set of variables *)
+  val empty : set
+
+  (** [next s] returns a variable of the set [s] and  the remaining set *)
+  val next : set -> t * set
+end
+
+(** Submodule of the CSP handling values of the variables *)
+module type VALS = sig
+
+  (** The type of values *)
+  type t
+
+  type set
+
+  val emtpy : set
+
+  val next : set -> t * set
+end
+
+(** Submodule of the CSP, handles a collection of variables with their
+    associated values *)
+module type INSTS = sig
+
+  type t
+
+  val empty : t
+end
+
 (** Signature of a CSP, input of the functor *)
 module type CSPS =
   sig
-    (** Variables which are defined in a domain *)
-    type x
 
-    (** Values of the variables *)
-    type v
+    module Var : VARS
 
-    (** Instantation of the csp *)
-    type instance
+    module Val : VALS
 
-    (** The empty instance *)
-    val empty : instance
-
-    (** List of variables in the problem *)
-    val vars : x list
+    module Instance : INSTS
 
     (** [feasible x v a] checks whether the instance [a] is consistent with
         constraint [x = v] *)
-    val feasible : x -> v -> instance -> bool
+    val feasible : Var.t -> Val.t -> Instance.t -> bool
 
     (** [consistent i] asserts whether instance [i] is consistent. Very close
         from [feasible] *)
-    val consistent : instance -> bool
+    val consistent : Instance.t -> bool
 
     (** [union a x v] returns a new instance of [a] with [x = v] *)
-    val union : instance -> x -> v -> instance
+    val union : Instance.t -> Var.t -> Val.t -> Instance.t
 
     (** [domain x] returns the (discrete) domain on which [x] is defined *)
-    val domain : x -> v list
+    val domain : Var.t -> Val.set
 
     (** [print i] offers a representation of instance [i] *)
-    val print : instance -> unit
+    val print : Instance.t -> unit
   end
 
 (** [Make(c)] returns appropriate functions to solve the csp problem [c] *)
 module Make : functor (Csp : CSPS) ->
     sig
       (** [bt v a] executes backtrack of variables [v] from instance [a] *)
-      val bt : Csp.x list -> Csp.instance -> Csp.instance
+      val bt : Csp.Var.set -> Csp.Instance.t -> Csp.Instance.t
     end

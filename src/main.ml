79ad9166386725ec482c78ba594566f246aa665e
gabrielhdt <gabrielhondet@gmail.com>
2018-02-22 12:57:15 +0100

    (+) prints initial sudoku if not instantiable

--- src/main.ml
+++ src/main.ml
@@ -5,11 +5,15 @@ let () =
   Queenspb.print final_inst
      *)
   let module Sudokubt = Backtrack.Make(Sudoku) in
-  let fi, nv = Sudoku.load "data/sudoku1.json" in
+  let fi, nv = Sudoku.load Sys.argv.(1) in
   if Sudoku.consistent fi then
     begin
       Sudoku.print fi ;
       let final_inst = Sudokubt.bt nv fi in
       Sudoku.print final_inst
     end
-  else failwith "initial problem not consistent"
+      else
+        begin
+          Sudoku.print fi ;
+          failwith "initial problem not consistent"
+        end
